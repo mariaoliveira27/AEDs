7fc95dcda2ee7e5869a121ded89ce829c334c9e5
Maria Clara <mariaclariagomes@gmail.com>
2024-06-19 11:55:27 -0300

    Create MARATONA prj_Cartas.c

---
+++ MARATONA prj_Cartas.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int cartas(){
+  
+}
+
+int main(void) {
+  int numeros[5];
+  int D = 0;
+  int C = 0;
+  int N = 0;
+  int sequencia = 0;
+  for(int i = 0; i < 5; i++){
+  printf("Digite 5 numeros de (1a13): ");
+  scanf("%d", &numeros);
+    }
+ 
+    if(numeros < sequencia){
+      printf("D");
+    }else if(numeros< sequencia){
+      printf("N");
+    }else{
+      printf("C");
+    }
+  
+  
+  return 0;
+}
